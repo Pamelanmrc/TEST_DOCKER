27a1be708ad72f2d273161aaf4ef61aa89b85561
Pamelanmrc <pamelamishra@gmail.com>
2025-11-10 06:37:01 -0500

    test

---
+++ load_stuff.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+module load Java/17.0.6
+module load singularity
+module load STAR
+module load SAMtools
+module load RSEM
+module load Java/17.0.6
+
---
+++ run_nextflow.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#If reference folder was downloaded in the same directory 
+#as nextflow. If not, change the --genomeDir and --rsem_ref
+    nextflow run main_final.nf \
+    -c  ./nextflow.ALIGN.config \
+    --samplesheet ./assets/samplesheet.example.csv \
+    --read_group TEST  \
+    --outdir ./TEST_HPC_runs  \
+    --genomeDir ./reference/Mouse/Reference/mm10_gencode/mm10STAR/  \
+    --rsem_ref ./reference/Mouse/rsem_index/rsem_mm10  \
+    --threads 10 -profile singularity 
+
